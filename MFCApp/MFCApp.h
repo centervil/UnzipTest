bc7f532e872c9a3a3aed135addd75681f4c0a646
ak-nkmr <centervil.en@gmail.com>
2019-07-12 00:52:59 +0900

    Create MFCAppProject

---
+++ MFCApp/MFCApp.h
@@ -0,0 +1,32 @@
+﻿
+// MFCApp.h : PROJECT_NAME アプリケーションのメイン ヘッダー ファイルです
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH に対してこのファイルをインクルードする前に 'pch.h' をインクルードしてください"
+#endif
+
+#include "resource.h"		// メイン シンボル
+
+
+// CMFCAppApp:
+// このクラスの実装については、MFCApp.cpp を参照してください
+//
+
+class CMFCAppApp : public CWinApp
+{
+public:
+	CMFCAppApp();
+
+// オーバーライド
+public:
+	virtual BOOL InitInstance();
+
+// 実装
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCAppApp theApp;
